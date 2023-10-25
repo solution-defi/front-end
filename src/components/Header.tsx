609644c0b081163244b71eee78f82a378d6329c9
yyyljy <yyyljy@gmail.com>
2023-10-26 00:07:29 +0900

    front page first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from "react";
-import { Flex, Menu, Image } from "@chakra-ui/react";
+import { ReactNode } from 'react';
+import { Flex, Menu, Image } from '@chakra-ui/react';
 
-import logo from "../assets/logo.svg";
+import logo from '../assets/logo.svg';
 
 type Props = {
   children?: ReactNode;
@@ -9,20 +9,8 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <Menu>
-      <Flex
-        d="flex"
-        alignItens="center"
-        justifyContent="space-between"
-        maxW="83.43rem"
-        mx="auto"
-        mt="1.5rem">
-        <Image
-          boxSize="1.56rem"
-          src={logo}
-          alt="Uniswap Logo" />
-        {children}
-      </Flex>
-    </Menu>
+    <>
+      <Menu>{children}</Menu>
+    </>
   );
 }
--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -1,15 +1,8 @@
-import {
-  Flex,
-  Box,
-  Image,
-  Text,
-  Button,
-  Input
-} from "@chakra-ui/react";
+import { Flex, Box, Image, Text, Button, Input } from '@chakra-ui/react';
 
-import { SettingsIcon, ChevronDownIcon, ArrowDownIcon } from '@chakra-ui/icons'
+import { SettingsIcon, ChevronDownIcon, ArrowDownIcon } from '@chakra-ui/icons';
 
-import etherLogo from '../assets/etherLogo.png'
+import etherLogo from '../assets/etherLogo.png';
 
 export default function Trade() {
   return (
@@ -18,37 +11,37 @@ export default function Trade() {
       mx="auto"
       mt="5.25rem"
       boxShadow="rgb(0 0 0 / 8%) 0rem 0.37rem 0.62rem"
-      borderRadius="1.37rem">
+      borderRadius="1.37rem"
+    >
       <Flex
         alignItems="center"
         p="1rem 1.25rem 0.5rem"
         bg="white"
         color="rgb(86, 90, 105)"
         justifyContent="space-between"
-        borderRadius="1.37rem 1.37rem 0 0">
-        <Text
-          color="black"
-          fontWeight="500">
+        borderRadius="1.37rem 1.37rem 0 0"
+      >
+        <Text color="black" fontWeight="500">
           Swap
         </Text>
         <SettingsIcon
           fontSize="1.25rem"
           cursor="pointer"
-          _hover={{ color: "rgb(128,128,128)" }}
+          _hover={{ color: 'rgb(128,128,128)' }}
         />
       </Flex>
 
-      <Box
-        p="0.5rem"
-        bg="white"
-        borderRadius="0 0 1.37rem 1.37rem">
+      <Box p="0.5rem" bg="white" borderRadius="0 0 1.37rem 1.37rem">
         <Flex
           alignItems="center"
           justifyContent="space-between"
           bg="rgb(247, 248, 250)"
           p="1rem 1rem 1.7rem"
-          borderRadius="1.25rem" border="0.06rem solid rgb(237, 238, 242)"
-          _hover={{ border: "0.06rem solid rgb(211,211,211)" }}>
+          borderRadius="1.25rem"
+          border="0.06rem solid rgb(237, 238, 242)"
+          color="#5e5e5e"
+          _hover={{ border: '0.06rem solid #940B92' }}
+        >
           <Box>
             <Button
               bg="white"
@@ -56,8 +49,12 @@ export default function Trade() {
               boxShadow="rgb(0 0 0 / 8%) 0rem 5.25rem 0.62rem"
               fontWeight="500"
               mr="0.5rem"
-              rightIcon={<ChevronDownIcon fontSize="1.37rem" cursor="pointer" />}>
-              <Image boxSize="1.5rem"
+              rightIcon={
+                <ChevronDownIcon fontSize="1.37rem" cursor="pointer" />
+              }
+            >
+              <Image
+                boxSize="1.5rem"
                 src={etherLogo}
                 alt="Ether Logo"
                 mr="0.5rem"
@@ -86,19 +83,27 @@ export default function Trade() {
           alignItems="center"
           justifyContent="space-between"
           bg="rgb(247, 248, 250)"
-          pos="relative" p="1rem 1rem 1.7rem"
+          pos="relative"
+          p="1rem 1rem 1.7rem"
           borderRadius="1.25rem"
           mt="0.25rem"
           border="0.06rem solid rgb(237, 238, 242)"
-          _hover={{ border: "0.06rem solid rgb(211,211,211)" }}>
+          _hover={{ border: '0.06rem solid #940B92' }}
+        >
           <Box>
             <Button
-              bg="rgb(232, 0, 111)"
-              color="white"
+              bg="#DA0C81"
+              color="rgb(255, 240, 250)"
               p="0rem 1rem"
               borderRadius="1.12rem"
               boxShadow="rgb(0 0 0 / 8%) 0rem 5.25rem 0.62rem"
-              _hover={{ bg: "rgb(207, 0, 99)" }} rightIcon={<ChevronDownIcon fontSize="1.37rem" cursor="pointer" />}>Select a token</Button>
+              _hover={{ bg: 'rgb(207, 0, 99)' }}
+              rightIcon={
+                <ChevronDownIcon fontSize="1.37rem" cursor="pointer" />
+              }
+            >
+              Select a token
+            </Button>
           </Box>
           <Flex
             alignItems="center"
@@ -108,11 +113,13 @@ export default function Trade() {
             borderRadius="0.75rem"
             pos="relative"
             top="-2.37rem"
-            left="2.5rem">
+            left="2.5rem"
+          >
             <ArrowDownIcon
               bg="rgb(247, 248, 250)"
               color="rgb(128,128,128)"
-              h="1.5rem" width="1.62rem"
+              h="1.5rem"
+              width="1.62rem"
               borderRadius="0.75rem"
             />
           </Flex>
@@ -127,22 +134,24 @@ export default function Trade() {
               outline="none"
               border="none"
               focusBorderColor="none"
-              type="number" />
+              type="number"
+            />
           </Box>
         </Flex>
 
         <Box mt="0.5rem">
           <Button
-            color="rgb(213, 0, 102)"
-            bg="rgb(253, 234, 241)"
+            color="rgb(255, 240, 250)"
+            bg="#940B92"
             width="100%"
             p="1.62rem"
             borderRadius="1.25rem"
-            _hover={{ bg: "rgb(251, 211, 225)" }}>
+            _hover={{ bg: '#E95793' }}
+          >
             Connect Wallet
           </Button>
         </Box>
       </Box>
     </Box>
-  )
+  );
 }
